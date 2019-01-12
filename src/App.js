cbc9dd829d4af2f4f0a6d598c2436a05da148e82
abigger87@gmail.com <abigger87@gmail.com>
2019-01-12 01:32:11 -0800

    landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React, { Component } from "react";
 import { Route, Redirect, Switch } from "react-router-dom";
-import "./App.css";
 import LoginForm from "./components/loginForm";
+import Home from "./components/home";
+import NotFound from "./components/notFound";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Switch>
         <Route path="/login" component={LoginForm} />
-        <Redirect from="/" exact to="/login" />
+        <Route path="/home" component={Home} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect from="/" exact to="/home" />
+        <Redirect to="/not-found" />
       </Switch>
     );
   }
---
+++ src/components/home.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div
+        style={{
+          textAlign: "center",
+          verticalAlign: "middle",
+          display: "table-cell"
+        }}
+      >
+        <h1 className="center m-4" style={{ color: "white" }}>
+          SNAP-VOTE
+        </h1>
+        <div id="my-login-button-target" />
+      </div>
+    );
+  }
+}
+
+export default Home;
