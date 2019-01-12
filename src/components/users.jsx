43a0c9bfd0f83bd83555e2b44eba3e0fd3248f85
abigger87@gmail.com <abigger87@gmail.com>
2019-01-12 12:24:20 -0800

    we nearly there

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -1,11 +1,46 @@
 import React, { Component } from "react";
 
 class Users extends Component {
+  componentDidMount() {}
+
   render() {
+    const btnStyle = {
+      borderWidth: "2px",
+      borderColor: "black",
+      color: "black"
+    };
     return (
-      <React.Fragment>
-        <img src={localStorage.getItem("userSnap")}></img>
-      </React.Fragment>
+      <div
+        style={{
+          textAlign: "center",
+          verticalAlign: "middle",
+          display: "table-cell"
+        }}
+      >
+        <h1 className="mb-4">Better School?</h1>
+        <div style={{ textAlign: "center" }}>
+          <div className="mb-4">
+            <button
+              className="btn m-2 btn-lg btn-outline-danger"
+              style={btnStyle}
+            >
+              USC
+            </button>
+            <button
+              className="btn m-2 btn-lg btn-outline-primary"
+              style={btnStyle}
+            >
+              UCLA
+            </button>
+          </div>
+        </div>
+        <div>
+          <img
+            style={{ width: "100px" }}
+            src={localStorage.getItem("userSnap")}
+          />
+        </div>
+      </div>
     );
   }
 }
