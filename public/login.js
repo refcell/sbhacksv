b8fd7dfeb3a40953608c19df7ee60289f6aa6d5b
abigger87@gmail.com <abigger87@gmail.com>
2019-01-12 18:51:02 -0800

    repaired history

--- public/login.js
+++ public/login.js
@@ -12,7 +12,6 @@ window.snapKitInit = function () {
           localStorage.setItem("userSnap", data.data.me.bitmoji.avatar);
           localStorage.setItem("userName", data.data.me.displayName);
           console.log("User info:", data);
-
           await window.history.forward();
           window.location = "/users";
         })
